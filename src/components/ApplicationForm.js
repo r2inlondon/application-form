aa37dc12e55c2128aa1c641b36d0eb58c8051cd4
Arturo <53298840+r2inlondon@users.noreply.github.com>
2022-02-09 17:28:13 +0000

    Finally got working the firstName test

--- src/components/ApplicationForm.js
+++ src/components/ApplicationForm.js
@@ -1,5 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
+import PersonalDetails from "./PersonalDetails";
 
-const ApplicationForm = () => <h1>Application Form</h1>;
+const ApplicationForm = () => {
+  const [details, setDetails] = useState({ firstName: "", lastName: "" });
+
+  function setName(firstName) {
+    console.log(firstName);
+
+    setDetails({ firstName: firstName });
+
+    console.log("details state is:", details);
+  }
+
+  return (
+    <div>
+      <h2>Application</h2>
+      <PersonalDetails data={setName} />
+    </div>
+  );
+};
 
 export default ApplicationForm;
--- src/components/PersonalDetails.js
+++ src/components/PersonalDetails.js
@@ -1,5 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 
-const PersonalDetails = () => <h3>Personal Details</h3>;
+const PersonalDetails = (props) => {
+  const [fName, setfName] = useState("");
+
+  const getFirstName = (e) => {
+    e.preventDefault();
+    setfName(e.target.value);
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    props.data(fName);
+  };
+
+  return (
+    <div className="personal-d">
+      <form onSubmit={onSubmit}>
+        <label>First Name</label>
+        <br />
+        <input
+          type="text"
+          id="fname"
+          name="fname"
+          value={fName}
+          onChange={getFirstName}
+        />
+        <br />
+        <input type="submit" value="Submit" />
+      </form>
+    </div>
+  );
+};
 
 export default PersonalDetails;
