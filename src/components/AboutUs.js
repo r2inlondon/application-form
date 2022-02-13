fab37427944194857b7fc4562c8f1fd99161820a
Arturo Quiroz <r2inlondon@gmail.com>
2022-02-13 14:45:29 +0000

    all 3 steps completed, now going to work on the clicks

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -1,9 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 
-const AboutUs = () => (
-  <div>
-    <h2>How did you hear about us?</h2>
-  </div>
-);
+const AboutUs = (props) => {
+  const [hear, setHear] = useState("");
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    props.setAboutUs({ hear });
+  };
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <label>How did you hear about us?</label>
+        <br />
+        <input
+          type="text"
+          id="hear"
+          name="hear"
+          value={hear}
+          onChange={(e) => setHear(e.target.value)}
+        />
+        <br />
+        <input type="submit" value="Submit"></input>
+      </form>
+    </div>
+  );
+};
 
 export default AboutUs;
--- src/components/ApplicationForm.js
+++ src/components/ApplicationForm.js
@@ -4,33 +4,73 @@ import AddressDetails from "./AddressDetails";
 import AboutUs from "./AboutUs";
 
 const ApplicationForm = () => {
-  const [details, setDetails] = useState({ opened: true });
-  const [addressDetails, setAddressDetails] = useState({ opened: false });
-  const [about, setAbout] = useState({ opened: false });
+  const [details, setDetails] = useState({
+    step1: {
+      active: true,
+      completed: false,
+    },
+    step2: {
+      active: false,
+      completed: false,
+    },
+    step3: {
+      active: false,
+      completed: false,
+    },
+  });
+
+  // const [addressDetails, setAddressDetails] = useState({ opened: false });
+  // const [about, setAbout] = useState({ opened: false });
   const [progress, setProgress] = useState([]);
 
   const step1 = (info) => {
     const { firstName, lastName } = info;
-    setDetails({ firstName, lastName, opened: false });
-    progress.push("Step 1");
-    console.log(progress);
-    setAddressDetails({ opened: true });
+
+    setDetails((prevState) => ({
+      step1: {
+        ...prevState.step1,
+        active: false,
+        completed: true,
+        firstName,
+        lastName,
+      },
+      step2: { ...prevState.step2, active: true },
+      step3: { ...prevState.step3 },
+    }));
   };
 
   const step2 = (info) => {
     const { firstLine, secondLine, city, country, postcode } = info;
 
-    setAddressDetails({
-      firstLine,
-      secondLine,
-      city,
-      country,
-      postcode,
-      opened: false
-    });
-    progress.push("Step 2");
-    console.log(progress);
-    setAbout({ opened: true });
+    setDetails((prevState) => ({
+      step1: { ...prevState.step1 },
+      step2: {
+        ...prevState.step2,
+        active: false,
+        completed: true,
+        firstLine,
+        secondLine,
+        city,
+        country,
+        postcode,
+      },
+      step3: { ...prevState.step3, active: true },
+    }));
+  };
+
+  const step3 = (info) => {
+    const { hear } = info;
+
+    setDetails((prevState) => ({
+      step1: { ...prevState.step1 },
+      step2: { ...prevState.step2 },
+      step3: {
+        ...prevState.step3,
+        active: false,
+        completed: true,
+        hear,
+      },
+    }));
   };
 
   console.log(progress);
@@ -38,14 +78,14 @@ const ApplicationForm = () => {
   return (
     <div>
       <div className="nav">
-        {progress.map((item, index) => {
-          return <button key={index}>{item}</button>;
-        })}
+        {details.step1.completed && <button key={1}>Personal Details</button>}
+        {details.step2.completed && <button key={2}>Home Address</button>}
+        {details.step3.completed && <button key={3}>Step 3</button>}
       </div>
       <h3>Application</h3>
-      {details.opened && <PersonalDetails setDetails={step1} />}
-      {addressDetails.opened && <AddressDetails setAddressDetails={step2} />}
-      {about.opened && <AboutUs />}
+      {details.step1.active && <PersonalDetails setDetails={step1} />}
+      {details.step2.active && <AddressDetails setAddressDetails={step2} />}
+      {details.step3.active && <AboutUs setAboutUs={step3} />}
     </div>
   );
 };
