6270e7f1fd1ebc062d8a17a0ea44714ee80d6d48
Arturo Quiroz <r2inlondon@gmail.com>
2022-02-16 12:19:39 +0000

    background image added, the-start-style.scss created, imported and basic style added

--- src/components/TheStart.js
+++ src/components/TheStart.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const TheStart = (props) => (
-  <div className="the-end">
+  <div className="the-start">
     <p>Start Application</p>
     <button onClick={props.TheStart}>Submit Form</button>
   </div>
