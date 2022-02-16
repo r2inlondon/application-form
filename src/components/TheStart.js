5ac16029468de62d36abbffc0dd693833ed98fc0
Arturo Quiroz <r2inlondon@gmail.com>
2022-02-16 15:14:45 +0000

    favicon icon added

--- src/components/TheStart.js
+++ src/components/TheStart.js
@@ -1,9 +1,13 @@
 import React from "react";
 
 const TheStart = (props) => (
-  <div className="the-start">
-    <p>Start Application</p>
-    <button onClick={props.TheStart}>Submit Form</button>
+  <div className="the-start bg">
+    <div className="start-txt">
+      <p>Job aplication form coded with React</p>
+      <button className="the-button" onClick={props.TheStart}>
+        Start
+      </button>
+    </div>
   </div>
 );
 
