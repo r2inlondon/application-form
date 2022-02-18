5f85f41a774c4da94205e971dee85ef8ffbbb925
Arturo Quiroz <r2inlondon@gmail.com>
2022-02-18 19:38:25 +0000

    progress button color and size have been adjusted, it looks better now

--- src/components/ProgressNav.js
+++ src/components/ProgressNav.js
@@ -16,47 +16,56 @@ const ProgressNav = ({ activeIt }) => {
                   id={1}
                   className="button-step"
                   onClick={activeIt}
-                >
-                  
-                </button>
+                ></button>
               )}
             </div>
             <div className="line"></div>
             <div className="steps">
               {details.step2.active && (
-                <button key={2} id={2} className="button-step" onClick={activeIt}>
-                 
-                </button>
+                <button
+                  key={2}
+                  id={2}
+                  className="button-step"
+                  onClick={activeIt}
+                ></button>
               )}
             </div>
             <div className="line"></div>
             <div className="steps">
               {details.step3.active && (
-                <button key={3} id={3} className="button-step" onClick={activeIt}>                  
-                </button>
+                <button
+                  key={3}
+                  id={3}
+                  className="button-step"
+                  onClick={activeIt}
+                ></button>
               )}
             </div>
             <div className="line"></div>
             <div className="steps">
               {details.step4.active && (
-                <button key={4} id={4} className="button-step" onClick={activeIt}>                  
-                </button>
+                <button
+                  key={4}
+                  id={4}
+                  className="button-step"
+                  onClick={activeIt}
+                ></button>
               )}
             </div>
           </div>
           <div className="progress-txt-container">
-              <div className="progress-txt">
-                <p>{details.step1.section}</p>
-              </div>
-              <div className="progress-txt">
-                <p>{details.step2.section}</p>
-              </div>
-              <div className="progress-txt">
-                <p>{details.step3.section}</p>
-              </div>
-              <div className="progress-txt">
-                <p>{details.step4.section}</p>
-              </div>
+            <div className="progress-txt">
+              <p>{details.step1.section}</p>
+            </div>
+            <div className="progress-txt">
+              <p>{details.step2.section}</p>
+            </div>
+            <div className="progress-txt">
+              <p>{details.step3.section}</p>
+            </div>
+            <div className="progress-txt">
+              <p>{details.step4.section}</p>
+            </div>
           </div>
         </div>
       )}
