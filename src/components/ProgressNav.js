47b5ded0a3e38af32bfb81093e886b73f6bbb869
Arturo <r2inlondon@gmail.com>
2022-02-18 17:45:59 +0000

    progress-nav basic display and functionality in place

--- src/components/ProgressNav.js
+++ src/components/ProgressNav.js
@@ -5,28 +5,62 @@ const ProgressNav = ({ activeIt }) => {
   const details = useContext(FormContext);
 
   return (
-    <div className="progress-nav">
-      <div className="steps">
-        {details.step1.active && (
-          <button key={1} id={1} className="step1" onClick={activeIt}>
-            {/* {details.step1.section} */}
-          </button>
-        )}
-      </div>
-      {details.step2.active && (
-        <button key={2} id={2} className="step2" onClick={activeIt}>
-          {details.step2.section}
-        </button>
-      )}
-      {details.step3.active && (
-        <button key={3} id={3} className="step3" onClick={activeIt}>
-          {details.step3.section}
-        </button>
-      )}
-      {details.step4.active && (
-        <button key={4} id={4} className="step4" onClick={activeIt}>
-          {details.step4.section}
-        </button>
+    <div>
+      {details.step1.active && (
+        <div className="nav">
+          <div className="progress">
+            <div className="steps">
+              {details.step1.active && (
+                <button
+                  key={1}
+                  id={1}
+                  className="button-step"
+                  onClick={activeIt}
+                >
+                  
+                </button>
+              )}
+            </div>
+            <div className="line"></div>
+            <div className="steps">
+              {details.step2.active && (
+                <button key={2} id={2} className="button-step" onClick={activeIt}>
+                 
+                </button>
+              )}
+            </div>
+            <div className="line"></div>
+            <div className="steps">
+              {details.step3.active && (
+                <button key={3} id={3} className="button-step" onClick={activeIt}>
+                  {details.step3.section}
+                </button>
+              )}
+            </div>
+            <div className="line"></div>
+            <div className="steps">
+              {details.step4.active && (
+                <button key={4} id={4} className="button-step" onClick={activeIt}>
+                  {details.step4.section}
+                </button>
+              )}
+            </div>
+          </div>
+          <div className="progress-txt-container">
+              <div className="progress-txt">
+                <p>{details.step1.section}</p>
+              </div>
+              <div className="progress-txt">
+                <p>{details.step2.section}</p>
+              </div>
+              <div className="progress-txt">
+                <p>{details.step3.section}</p>
+              </div>
+              <div className="progress-txt">
+                <p>{details.step4.section}</p>
+              </div>
+          </div>
+        </div>
       )}
     </div>
   );
